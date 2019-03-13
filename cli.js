3138c982aa7f5c8c82c72c8f990c46016d65f91c
Joe Reed <joe.reed.4@gmail.com>
2019-03-13 13:07:31 +0000

    Refactors into CLI tool

---
+++ cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const fs = require('fs-extra');
+const { argv } = require('yargs');
+const nowConfig = require('./now.json');
+
+const [shortcut, url] = argv._;
+const newConfig = {...nowConfig };
+
+const newRedirect = {
+    src: `/${shortcut}`,
+    status: 301,
+    headers: { Location: url },
+};
+
+newConfig.routes.splice(newConfig.routes.length - 1, 0, newRedirect);
+
+if (!argv.test) {
+    fs.writeFileSync(
+        './now.json',
+        JSON.stringify(newConfig, null, 4)
+    );
+}
+else {
+    console.log(JSON.stringify(newConfig, null, 4));
+}
